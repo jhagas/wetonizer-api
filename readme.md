a613f61301dae0cc4c8dbfe315e2bb935dddc53d
Jhagas Winaya <jhagashana@mailbox.org>
2022-01-13 23:37:14 +0700

    almost done

--- readme.md
+++ readme.md
@@ -6,16 +6,20 @@
 -->
 
 # Javanese Calendar API
-Public API build in nodejs (express) that takes gregorian calendar input `yyyy-mm-dd` and return various javanese calendar value in input date.
+Public API build in nodejs (express) that takes gregorian calendar input `yyyy-mm-dd` and return various javanese calendar value corresponding to the input value it gets.
 
-I hardcode (as variable) the kurup (120 years cycle) to 24 March 1936, which the day when kurup Asapon began. Often time, the kurup cycle is determined by kasultanan yogya or mataram, so i think hardcoding it will be the best options.
+I hardcode (as variable) the kurup (120 years cycle) to 24 March 1936, which the day when kurup Asapon began. Often time, the kurup cycle is determined by kasultanan yogya or mataram, so i think hardcoding it will be the best option. The next kurup will happen in year 1986 Javanese (or maybe sooner). 
 
 More info about javanese calendar:
 1. [Wikipedia Bahasa Indonesia, kalender jawa](https://id.wikipedia.org/wiki/Kalender_Jawa)
 2. [Basic algorithm kalender jawa](http://kalenderimlek.blogspot.com/2017/11/warsa-lambang-dan-windu-dalam-kalender.html)
 
-## Algorithm
-Everything is calculated from the days from epoch time to input date. Using some declaration taken from Wikipedia article above, i can calculate any value in javanese calendar (except Kurup).
+## About Algorithm
+Although javanese calendar is hardly based on hijri (islamic) calendar. It's not really the same. So taking gregorian to hijri algorithm and rename the name of month to javanese one often time get a pretty bad result.
+
+Everything is calculated from the days from epoch time to input date. Using some declarations (or definitions) taken from Wikipedia article above, i can calculate any value in javanese calendar (except Kurup).
+
+In the code I use the term 'biasa' and 'kabisat'. Biasa (Javanese:wastu) is an usual year, and kabisat (Javanese:wuntu) is a leap year
 
 ## About `hariid/idHari` and `pasaranid/idPasaran`
 I suppose you know a little bit about javanese calendar from articles above. I take idPasaran and idHari from UNIX Epoch time (1 Jan 1970), not from Kurup Asapon Epoch.
@@ -35,9 +39,11 @@ I suppose you know a little bit about javanese calendar from articles above. I t
 https://domain/yyyy-mm-dd
 ```
 
-use GET request, CORS enabled. You can use in your project, whatever you want.
+use GET request, CORS enabled by default. You can use in your project, whatever you want.
 Just deploy in your own server to make sure everything run smoothly.
 
 ## Output (JSON Schema)
 
+
 ## Deployment
+I will be using Vercel as demonstration for this API. I predict this express server will be resource intensive (on large scale) because the number calculations it makes.
--- routes/javanese.js
+++ routes/javanese.js
@@ -1,3 +1,4 @@
+const e = require("express");
 var express = require("express");
 var router = express.Router();
 
@@ -42,7 +43,96 @@ const bulan = [
   "Desember",
 ];
 
-let jumlahHariTiapTahun = [354, 355, 354, 354, 355, 354, 354, 355];
+const jumlahHariTiapTahun = [354, 355, 354, 354, 355, 354, 354, 355];
+const kabisat = [30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 30];
+const biasa = [30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 29];
+const wulan = [
+  "Sura",
+  "Sapar",
+  "Mulud",
+  "Bakda Mulud",
+  "Jumadil Awal",
+  "Jumadil Akir",
+  "Rejeb",
+  "Ruwah",
+  "Pasa",
+  "Sawal",
+  "Séla",
+  "Besar",
+];
+const windu = ["Adi", "Kuntara", "Sengara", "Sancaya"];
+const lambang = ["Kelawu", "Langkir"];
+
+const mangsaBiasa = [32, 25, 25, 24, 23, 41, 41, 23, 24, 25, 27, 43, 10];
+const mangsaKabisat = [32, 26, 25, 24, 23, 41, 41, 23, 24, 25, 27, 43, 10];
+const pranataMangsa = [
+  {
+    nama: "Kapitu (palguna)",
+    deskripsi:
+      "Benih padi mulai ditanam di sawah, banyak hujan, banyak sungai yang banjir. Penampakannya/ibaratnya : wisa kentar ing ing maruta (bisa larut dengan angin, itu masanya banyak penyakit). Musim banjir, badai longsor mulai tandur.",
+  },
+  {
+    nama: "Kawolu (wasika)",
+    deskripsi:
+      "Padi mulai hijau, uret mulai banyak. Penampakannya/ibaratnya : anjrah jroning kayun (merata dalam keinginan, musimnya kucing kawin). Musim padi beristirahat, banyak ulat, banyak penyakit.",
+  },
+  {
+    nama: "Kasanga (jita)",
+    deskripsi:
+      "Padi mulai berkembang dan sebagian sudah berbuah, jangkrik mulai muncul, kucing mulai kawin, cenggeret mulai bersuara. Penampakannya/ibaratnya : wedaring wacara mulya ( binatang tanah dan pohon mulai bersuara). Musim padi berbunga, turaes (sebangsa serangga) ramai berbunyi.",
+  },
+  {
+    nama: "Kasadasa (srawana)",
+    deskripsi:
+      "Padi mulai menguning, mulai panen, banyak hewan hamil, burung-burung kecil mulai menetas telurnya. Penampakannya/ibaratnya : gedong minep jroning kalbu (masa hewan sedang hamil). Musim padi berisi tapi masih hijau, burung-burung membuat sarang, tanam palawija di lahan kering.",
+  },
+  {
+    nama: "Dhesta (pradawana)",
+    deskripsi:
+      "Seluruhnya memanen padi. Penampakannya/ibaratnya: sotya (anak burung) sinara wedi (disuapi makanan). Masih ada waktu untuk palawija, burung-burung menyuapi anaknya.",
+  },
+  {
+    nama: "Sadha (asuji)",
+    deskripsi:
+      "Para petani mulai menjemur padi dan memasukkan ke lumbung. Di sawah hanya tersisa dami. Penampakannya/ibaratnya : tirta (keringat) sah saking sasana (badan) (air pergi darisumbernya, masa ini musim dingin, jarang orang berkeringat, sebab sangat dingin). Musim menumpuk jerami,tanda-tanda udara dingin pada pagi hari.",
+  },
+  {
+    nama: "Kasa (kartika)",
+    deskripsi:
+      "Para petani membakar dami yang tertinggal di sawah dan di masa ini dimulai menanam palawija, sejenis belalang masuk ke tanah, daun-daunan berjatuhan. Penampakannya/ibaratnya : lir sotya (dedaunan) murca saka ngembanan (kayu-kayuan).",
+  },
+  {
+    nama: "Karo (poso)",
+    deskripsi:
+      "Palawija mulai tumbuh, pohon randu dan mangga, tanah mulai retak/berlubang. Penampakannya/ibaratnya : bantala (tanah) rengka (retak). Musim kapok bertunas tanam palawija kedua.",
+  },
+  {
+    nama: "Katelu",
+    deskripsi:
+      "Musimnya/waktunya lahan tidak ditanami, sebab panas sekali, yang mana Palawija mulai di panen, berbagai jenis bambu tumbuh. Penampakannya/ibaratnya : suta (anak) manut ing Bapa (lanjaran). Musim ubi-ubian bertunas panen palawija.",
+  },
+  {
+    nama: "Kapat (sitra)",
+    deskripsi:
+      "Sawah tidak ada (jarang) tanaman, sebab musim kemarau, para petani mulai menggarap sawah untuk ditanami padi gaga, pohon kapuk mulai berbuah, burung-burung kecil mulai bertelur. Penampakannya/ibaratnya : waspa kumembeng jroning kalbu (sumber). Musim sumur kering, kapuk berbuah, tanam pisang. Pada masa ini kemarau berakhir.",
+  },
+  {
+    nama: "Kalima (manggala)",
+    deskripsi:
+      "Mulai ada hujan, selokan sawah diperbaiki dan membuat tempat mengalir air di pinggir sawah, mulai menyebar padi gaga, pohon asem mulai tumbuh daun muda, ulat-ulat mulai keluar. Penampakannya/ibaratnya : pancuran (hujan) emas sumawur (hujannya) ing jagad. Musim turun hujan, pohon asam bertunas, pohon kunyit berdaun muda.",
+  },
+  {
+    nama: "Kanem (naya)",
+    deskripsi:
+      "Para petani mulai menyebar bibit tanaman padi di pembenihan, banyak buah-buahan (durian, rambutan, manggis dan lain-lainnya), burung blibis mulai kelihatan di tempat-tempat berair. Penampakannya/ibaratnya : rasa mulya kasucian (sedang banyak-banyaknya buah-buahan). Musim buah-buahan mulai tua, mulai menggarap sawah.",
+  },
+  {
+    nama: "Kapitu (palguna)",
+    deskripsi:
+      "Benih padi mulai ditanam di sawah, banyak hujan, banyak sungai yang banjir. Penampakannya/ibaratnya : wisa kentar ing ing maruta (bisa larut dengan angin, itu masanya banyak penyakit). Musim banjir, badai longsor mulai tandur.",
+  },
+];
+
 // Warsa is depended on kurup
 function WarsaInfo() {
   const alip = {
@@ -156,7 +246,7 @@ function AwalBulan(info) {
   const rejeb = [3, 3];
   const ruwah = [5, 3];
   const pasa = [6, 2];
-  const syawal = [1, 2];
+  const sawal = [1, 2];
   const dulkaidah = [2, 1];
   const dulkijah = [4, 1];
 
@@ -164,18 +254,18 @@ function AwalBulan(info) {
     sapar: `Sapar, ${hari[(info.satuSura.idHari + sapar[0] - 1) % 7]["hari"]} ${
       pasaran[(info.satuSura.idPasaran + sapar[1] - 1) % 5]["pasaran"]
     }`,
-    mulud: `Mulud/Rabiulawal, ${
+    mulud: `Mulud/Rabiul awal, ${
       hari[(info.satuSura.idHari + mulud[0] - 1) % 7]["hari"]
     } ${pasaran[(info.satuSura.idPasaran + mulud[1] - 1) % 5]["pasaran"]}`,
-    rabiulakir: `Badamulud/Rabiulakir, ${
+    rabiulakir: `Bakda mulud/Rabiul akir, ${
       hari[(info.satuSura.idHari + rabiulakir[0] - 1) % 7]["hari"]
     } ${pasaran[(info.satuSura.idPasaran + rabiulakir[1] - 1) % 5]["pasaran"]}`,
-    jumadilawal: `Jumadilawal, ${
+    jumadilawal: `Jumadil awal, ${
       hari[(info.satuSura.idHari + jumadilawal[0] - 1) % 7]["hari"]
     } ${
       pasaran[(info.satuSura.idPasaran + jumadilawal[1] - 1) % 5]["pasaran"]
     }`,
-    jumadilakir: `Jumadilakir, ${
+    jumadilakir: `Jumadil akir, ${
       hari[(info.satuSura.idHari + jumadilakir[0] - 1) % 7]["hari"]
     } ${
       pasaran[(info.satuSura.idPasaran + jumadilakir[1] - 1) % 5]["pasaran"]
@@ -190,15 +280,40 @@ function AwalBulan(info) {
       pasaran[(info.satuSura.idPasaran + pasa[1] - 1) % 5]["pasaran"]
     }`,
     syawal: `Syawal, ${
-      hari[(info.satuSura.idHari + syawal[0] - 1) % 7]["hari"]
-    } ${pasaran[(info.satuSura.idPasaran + syawal[1] - 1) % 5]["pasaran"]}`,
+      hari[(info.satuSura.idHari + sawal[0] - 1) % 7]["hari"]
+    } ${pasaran[(info.satuSura.idPasaran + sawal[1] - 1) % 5]["pasaran"]}`,
     dulkaidah: `Séla/Dulkaidah, ${
       hari[(info.satuSura.idHari + dulkaidah[0] - 1) % 7]["hari"]
     } ${pasaran[(info.satuSura.idPasaran + dulkaidah[1] - 1) % 5]["pasaran"]}`,
-    dulkijah: `Dulkijah, ${
+    dulkijah: `Besar/Dulkijah, ${
       hari[(info.satuSura.idHari + dulkijah[0] - 1) % 7]["hari"]
     } ${pasaran[(info.satuSura.idPasaran + dulkijah[1] - 1) % 5]["pasaran"]}`,
   };
+}
+
+function Mangsa(unix) {
+  let year = unix.getFullYear();
+  let Rentang = Math.floor(
+    (unix.getTime() - new Date(`${year}`).getTime()) / 1000 / 3600 / 24
+  );
+  let leap = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+
+  let P = 0
+  let A = Rentang - 32;
+
+  if (leap) {
+    while (A > 0) {
+      P += 1;
+      A -= mangsaBiasa[P];
+    }
+  } else {
+    while (A > 0) {
+      P += 1;
+      A -= mangsaKabisat[P];
+    }
+  }
+
+  return pranataMangsa[P]
 }
 
 function Weton(unix) {
@@ -223,29 +338,54 @@ function Neptu(unix) {
   return neptu;
 }
 
-function Warsa(unix) {
+function Jawa(unix) {
   let date = unix.getTime();
   let Rentang = Math.floor((date - epoch) / 1000 / 3600 / 24);
   let A = Rentang % 2835;
 
+  let B = 0;
   let I = 0;
-  let x = A - jumlahHariTiapTahun[I];
+  let x = A - jumlahHariTiapTahun[I] + 1;
 
   while (x > 0) {
     I += 1;
     x -= jumlahHariTiapTahun[I];
   }
+  let D = x + jumlahHariTiapTahun[I] - 30;
+  let H = D + 30;
+
+  if (jumlahHariTiapTahun[I] == 355) {
+    while (D > 0) {
+      H = D;
+      B += 1;
+      D -= kabisat[B];
+    }
+  }
+  if (jumlahHariTiapTahun[I] == 354) {
+    while (D > 0) {
+      H = D;
+      B += 1;
+      D -= biasa[B];
+    }
+  }
 
   let warsaInfo = WarsaInfo();
   let info = warsaInfo[I];
   let awal = AwalBulan(info);
+  let mangsa = Mangsa(unix);
 
   let T = Math.floor(Rentang / 2835) * 8 + kurupTahun + I;
+  let L = Math.floor((((Rentang % 5670) + 2835) % 5670) / 2835);
+
+  let winduNow = Math.floor((Rentang % 11340) / 2535);
 
   return {
-    tahun: { numerik: T, warsa: info, bulan: awal },
-    bulan: "Sapar",
-    tanggal: "14",
+    tanggal: H,
+    bulan: wulan[B],
+    tahun: { numerik: T, warsa: info, awalBulan: awal },
+    windu: windu[winduNow],
+    lambang: lambang[L],
+    mangsa,
   };
 }
 
@@ -255,7 +395,7 @@ router.get("*", function (req, res) {
 
   let weton = Weton(unix);
   let neptu = Neptu(unix);
-  let jawa = Warsa(unix);
+  let jawa = Jawa(unix);
 
   res.json({
     pasaran: { weton: weton, neptu: neptu },
